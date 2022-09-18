9316575173c2b7e5d5e7e4bdbf3bfa79f806ec58
Olga Travina <110350045+trols@users.noreply.github.com>
2022-09-19 01:59:15 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,111 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { data } from './data';
+import mainfoto from './mainfoto.jpeg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const[cafes,setCafes]=useState(data);
+const[showText,setShowText]=useState(false);
+
+const removeCafes = (id)=>{
+  let newCafes=cafes.filter(cafe=>cafe.id !== id);
+  setCafes(newCafes)
 }
+const [foto,setFoto]=useState(0);
+const[frames,setFrames]=useState(data);
+let newArr=[...frames];
+
+const previousFoto=(e,index)=>{
+  const slide = document.querySelectorAll("#slide");
+  newArr[index]=cafes[index];
+  setFrames(newArr);
+  setFoto((foto=>{
+    foto -- ;
+    if(foto < 0){
+      foto=2;
+      
+    }   
+     return foto;
+  }))
+  slide[index].setAttribute('src',newArr[index].image[foto]);
+  return slide;
+}
+
+const nextFoto=(e, index)=>{
+setFrames(newArr);
+const slide = document.querySelectorAll("#slide");
+newArr[index]=cafes[index];
+setFoto((foto => {
+  foto ++ ;
+  if(foto > 2){
+    foto=0;
+  }
+  return foto;
+}))
+slide[index].setAttribute('src' ,newArr[index].image[foto]);
+return slide;}
+
+const showTextClick=(item)=>{
+  item.showMore =! item.showMore
+ setShowText(!showText)
+}
+
+return(
+    <div className='rt'>
+
+        <div className='container'>
+          <h2>5 ЛУЧШИХ КОНДИТЕРСКИХ</h2>
+          </div>
+          <div className='container'>
+          <h2>Санкт-Петербурга</h2>
+        </div>
+          <div className='container'>
+        <img src= { mainfoto } alt='fon' width='300px' className='maple'/>
+           </div>
+           <div className='container'>
+          <h3>Осталось посетить {cafes.length}</h3>
+        </div>
+       {cafes.map((item =>{
+            const{id,cafeName,check,image,description,source,showMore} = item;
+            const index = cafes.indexOf(item);
+
+            return(
+              
+              <div key={id} data-id={data.id}>
+
+              <div className='container'>
+          <h1>{id}.{cafeName}</h1>
+        </div>
+       <div className='container'>
+        <img src = { image[0]} alt='ph' width='369px' height='250px'id='slide'/>
+        </div>
+        <div className='container'>
+          <button onClick={ (e)=>previousFoto(e, index)} id={id} className='side'>НАЗАД</button>
+          <button onClick={ (e)=>nextFoto(e, index)} id={id} className='side'>ВПЕРЕД</button>
+           </div>
+<div className='container'>
+           <p>
+            <img  src={check} alt='ph' width='40px'/>
+          {showMore ? description : description.substring(0,25)}
+          <button className='about' onClick={()=>showTextClick(item)}>{showMore ? "Свернуть" : "...Узнать больше"}</button>
+          </p> 
+        </div> 
+       
+        <div className='container'>
+          <p className='par'>Источник : {source}</p> 
+          </div>
+<div className='container'>
+          <button className='btn' onClick={()=>removeCafes(id)}>УДАЛИТЬ</button>
+        </div>
+       </div>
+            )
+            }))}
+        
+        <div className='container'>
+        <button id='lastbtn' onClick={()=>setCafes([])}>Удалить все</button>
+        </div>
+        </div>
+  );
+      }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,69 @@
+import  konditer1  from './konditer1.jpeg';
+import  konditer2 from './konditer2.jpeg';
+import  konditer3 from './konditer3.jpeg';
+import  konditer4  from './konditer4.jpeg';
+import  konditer5 from './konditer5.jpeg';
+import  konditer6 from './konditer6.jpeg';
+import  konditer7  from './konditer7.jpeg';
+import  konditer8 from './konditer8.jpeg';
+import  konditer9 from './konditer9.jpeg';
+import  konditer10 from './konditer10.jpeg';
+import  konditer11 from './konditer11.jpeg';
+import  konditer12 from './konditer12.jpeg';
+import  konditer13 from './konditer13.webp';
+import  konditer14 from './konditer14.webp';
+import  konditer15 from './konditer15.webp';
+
+export const data = [ 
+    {
+        id: 1,
+            cafeName: "Буше",
+            image: [konditer1, konditer2, konditer3],
+            check: "//img.icons8.com/external-konkapp-outline-color-konkapp/2x/external-cup-kitchen-konkapp-outline-color-konkapp.png",
+            description: "Более 60 атмосферных мест в Петербурге и Москве.Мы создаем, а не потребляем. Готовимся развивать творческий кластер, объединяя людей. Мы — место, где жизнь. Мы делимся теплом. Здесь комфортно творить Мы изначально выбрали ракурс на потребности человека, а не на прибыль.",
+            source: "https://bushe.ru",
+            showMore: false
+            },
+    {
+        id: 2,
+        cafeName: "Забыли Сахар",
+       image: [konditer4, konditer5, konditer6],
+        check: "//img.icons8.com/external-konkapp-outline-color-konkapp/2x/external-cup-kitchen-konkapp-outline-color-konkapp.png",
+        description: "Забыли Сахар — это баланс вкуса и пользы.который мы соблюдаем за счёт употребления минимального количества сахара или полного исключения его из рецепта, а так же за счёт уменьшения калорийности, путём замены продуктов на более легкие, натуральные и полезные.В нашем меню вы найдете веганские, сыроедческие, фитнес десерты, а так же десерты без сахара и глютена .",
+       source: "https://забылисахар.рф",
+       showMore: false
+       
+    
+    },
+    
+        {
+            id: 3,
+                cafeName: "Британские пекарни",
+               image: [konditer7, konditer8, konditer9],
+                check: "//img.icons8.com/external-konkapp-outline-color-konkapp/2x/external-cup-kitchen-konkapp-outline-color-konkapp.png",
+                description: "Сегодня «Британские пекарни» — это интернет-магазин и более 20 кафе в Санкт-Петербурге, где всегда можно попробовать свежеиспеченный ремесленный хлеб, разнообразную выпечку, торты, пирожные, блюда от шефа, ароматный кофе и традиционный английский чай.",
+                source: "https://british-bakery.ru",
+                showMore: false
+                },
+                {
+                    id: 4,
+                        cafeName: "Север   Метрополь",
+                        image: [konditer10,konditer11,konditer12],
+                        check: "//img.icons8.com/external-konkapp-outline-color-konkapp/2x/external-cup-kitchen-konkapp-outline-color-konkapp.png",
+                        description: "Мы делаем все для того, чтобы вы могли приобрести любимые пирожные и торты в любой точке города и были довольны их вкусом, составом и свежестью.",
+                        source: "https://sever-metropol.ru",
+                        showMore: false
+                    },
+                    {
+                        id: 5,
+                            cafeName: "Кондитерская Анны Красовской",
+                            image: [konditer13,konditer14,konditer15],
+                            check: "//img.icons8.com/external-konkapp-outline-color-konkapp/2x/external-cup-kitchen-konkapp-outline-color-konkapp.png",
+                            description: "Кондитерская задумывалась как маленькая французская кафешка «для своих». Она находится вдалеке от шумных туристических улиц и здесь всего девять столиков.  Но уже в первый год работы Кондитерская получила несколько престижных наград и вошла в тройку лучших кафе Петербурга. Секрет в удивительной атмосфере и авторских десертах, которые меняются каждый сезон.",
+                            source: "https://krasovskaia.com",
+                            showMore: false
+                        }
+                    
+                
+        ]
+    
